84bafb48a27115c3156dfc9d656aa9cb69bc4976
rhea <rheasaldanha@rheas-air>
2017-05-09 12:28:55 +0530

    Version 4[Junit testSuite added for 2methods]- From 1503

---
+++ isa1/AllTests.java
@@ -0,0 +1,11 @@
+package isa1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CarTest.class, NameTest.class })
+public class AllTests {
+
+}
